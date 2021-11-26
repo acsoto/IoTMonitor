e297a45fcda1c42b60b929f61bf253320d856dd1
soto <z@mcac.cc>
2021-11-26 19:37:23 +0800

    Initial Commit

---
+++ IoTMonitor/IoTMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IoTMonitorApp.swift
+//  IoTMonitor
+//
+//  Created by 周子航 on 2021/11/26.
+//
+
+import SwiftUI
+
+@main
+struct IoTMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
