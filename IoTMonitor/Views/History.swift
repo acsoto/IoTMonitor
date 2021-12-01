0450d01e9cf5eac4e65945063032d7a78b0101ae
soto <z@mcac.cc>
2021-12-01 10:30:55 +0800

    更改历史曲线样式，使其更符合卡片设计

--- IoTMonitor/Views/History.swift
+++ IoTMonitor/Views/History.swift
@@ -10,7 +10,7 @@ import SwiftUICharts
 
 struct History: View {
     @EnvironmentObject var postManager: PostManager
-    @State var date1 = Date()
+    @State var date1 = NSDate.init(timeInterval: -2 * 24 * 60 * 60, since: Date()) as Date
     @State var date2 = Date()
 
     var body: some View {
@@ -18,17 +18,36 @@ struct History: View {
             ScrollView {
                 TimePickCard(date1: $date1, date2: $date2)
                 Button(action: {
-                    postManager.getHistory(begin: String(format: "%.0f", date1.timeIntervalSince1970 * 1000), end: String(format: "%.0f", date2.timeIntervalSince1970 * 1000))
+                    postManager.getHistory(begin: getDateString(date: date1), end: getDateString(date: date2))
                 }, label: {
                     Card(icon: Image(systemName: "repeat"), str1: Text(""), str2: Text("获取").foregroundColor(.cyan), str3: Text(""))
                 })
-                Spacer(minLength: 50)
-                HStack {
-                    LineChartView(data: postManager.temperatureHistory, title: "温度")
-                    LineChartView(data: postManager.humidityHistory, title: "湿度")
-                }
-            }.navigationTitle("历史记录")
+//                MultiLineChartView(data: [(postManager.temperatureHistory,GradientColors.orange),(postManager.humidityHistory,GradientColors.blue)], title: "", form: ChartForm.large)
+//                    .cornerRadius(20)
+//                    .overlay(
+//                            RoundedRectangle(cornerRadius: 10)
+//                                .stroke(Color(.sRGB, red: 150 / 255, green: 150 / 255, blue: 150 / 255, opacity: 0.1), lineWidth: 0.5)
+//                    )
+//                    .padding([.top, .horizontal])
+                LineChartView(data: postManager.temperatureHistory, title: "温度", form: ChartForm.large)
+                        .cornerRadius(20)
+                        .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color(.sRGB, red: 150 / 255, green: 150 / 255, blue: 150 / 255, opacity: 0.1), lineWidth: 0.5)
+                        )
+                        .padding([.top, .horizontal])
+                LineChartView(data: postManager.humidityHistory, title: "湿度", form: ChartForm.large)
+                        .cornerRadius(20)
+                        .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color(.sRGB, red: 150 / 255, green: 150 / 255, blue: 150 / 255, opacity: 0.1), lineWidth: 0.5)
+                        )
+                        .padding([.top, .horizontal])
+                        .onReceive(postManager.timer) { time in
 
+                        }
+
+            }.navigationTitle("历史记录")
         }
     }
 }
@@ -38,4 +57,8 @@ struct History_Previews: PreviewProvider {
         MultiLineChartView(data: [([8, 32, 11, 23, 40, 28], GradientColors.green), ([90, 99, 78, 111, 70, 60, 77], GradientColors.purple), ([34, 56, 72, 38, 43, 100, 50], GradientColors.orngPink)], title: "Title")
 
     }
+}
+
+func getDateString(date: Date) -> String {
+    return String(format: "%.0f", date.timeIntervalSince1970 * 1000)
 }
